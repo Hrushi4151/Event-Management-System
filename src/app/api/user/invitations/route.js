c64da156deaa3bc2593202e3c5c505b9345fc28d
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2026-01-28 15:42:40 +0530

    Update import path for authOptions in route.js

--- src/app/api/user/invitations/route.js
+++ src/app/api/user/invitations/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import connectDB from '../../../../utils/mongoose';
 import Registration from '../../../../models/Registration';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../auth/[...nextauth]/route.js';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export async function GET(req) {
   await connectDB();
@@ -45,4 +45,5 @@ export async function GET(req) {
     return NextResponse.json({ error: 'Failed to fetch invitations' }, { status: 500 });
   }
 }
+
 
